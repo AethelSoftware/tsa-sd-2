4dd9561e0e2b5a66ee04f9d8037e17c88370115a
JoshuaS5665 <jshah266507@gmail.com>
2025-11-15 10:27:49 -0500

    bous

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,18 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_pymongo import PyMongo
+from authlib.integrations.flask_client import OAuth
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb+srv://jshah26:tsasd2026@tsa-sd-2026.h9qb1rd.mongodb.net/TRYVER"
 
+# Instantiate the DB and OAuth authorization with our flask app. 
+
 mongo = PyMongo(app)
+oauth = OAuth(app)
+
+google = oauth.register()
+
 
 CORS(app, resources={r"/api/*": {"origins": "*"}})  # allow all origins for /api/*
 
