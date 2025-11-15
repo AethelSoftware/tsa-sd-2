e3d8284c0deca330aa76f6db647fd97418960e52
JoshuaS5665 <jshah266507@gmail.com>
2025-11-15 10:19:57 -0500

    basic db stuff

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://jshah26:tsasd2026@tsa-sd-2026.h9qb1rd.mongodb.net/TRYVER"
+
+mongo = PyMongo(app)
+
 CORS(app, resources={r"/api/*": {"origins": "*"}})  # allow all origins for /api/*
 
 @app.route("/api/hello")
