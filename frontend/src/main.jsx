c8a26d9824ed8a1cf40219c9cf037c028bf03ede
FrontendGuy <stehu06@gmail.com>
2025-11-17 17:00:15 -0500

    Made dummy map page (FRONTEND SHELL ONLY)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'; // 👈 Import 
 import './index.css';
 import App from './App.jsx';
 import Login from './Login.jsx';
+import DashboardPage from './Dashboard.jsx';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
